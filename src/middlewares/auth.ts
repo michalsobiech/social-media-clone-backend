1e21c67edfe4ae1edce33ea7952abe26d526250c
Michał Sobiech <michal.sobiech50@gmail.com>
2024-04-08 00:29:36 +0200

    feat: update auth.js middleware to match APIError new format

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,13 +1,17 @@
-import { type Request, type Response, type NextFunction } from 'express';
-import APIError from '../utils/APIError.js';
-import { UNAUTHORIZED } from '../constants/HttpStatusCodes.js';
+import { type Request, type Response, type NextFunction } from "express";
+import APIError from "../utils/APIError.js";
+import { UNAUTHORIZED } from "../constants/HttpStatusCodes.js";
 
 async function auth(req: Request, res: Response, next: NextFunction) {
   try {
     if (req.session.userId) {
       next();
     } else {
-      throw new APIError(UNAUTHORIZED, 'Unauthorized access');
+      throw new APIError({
+        status: UNAUTHORIZED,
+        title: "Unauthorized access",
+        detail: "Please login first",
+      });
     }
   } catch (error) {
     next(error);
